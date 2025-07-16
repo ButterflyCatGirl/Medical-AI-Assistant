3bc4daa2637c86fe43b97a31924fc095b14df95f
Marwa Moaen <51681169+ButterflyCatGirl@users.noreply.github.com>
2025-07-16 13:04:11 +0300

    Update medical_vqa.py

--- medical_vqa.py
+++ medical_vqa.py
@@ -153,7 +153,7 @@ def main():
                 col1, col2 = st.columns([1, 1])
                 
                 with col1:
-                    st.image(image, caption="Uploaded Medical Image", use_column_width=True)
+                    st.image(image, caption="Uploaded Medical Image", use_container_width=True)
                 
                 with col2:
                     # Question input
